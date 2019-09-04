e1aeff7fc689ca8a15851ff25bde243ed9aec985
samuelkaranja <samuelkaranja007@gmail.com>
2019-09-04 14:54:21 +0300

    File update

--- profile.php
+++ profile.php
@@ -5,65 +5,65 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Profile</title>
+	<title>Profile page</title>
 </head>
-<body style="background-color:;">
-	<h2 class="head4" style="text-align: center; font-weight: bold; padding-top: 40px; font-size: 40px;">My Profile</h2>
-	<center><img src="img/lg.jpg"  style="border-radius: 10px;"></center>
-	<div style="text-align: center;  margin: 10px 0px 20px 0px; font-size: 25px;">
-		<h4>
-			<?php echo "Welcome " . $_SESSION['login_user'];?>
-		</h4>
-	</div>
+<body>
+	<?php
+		$sql = "SELECT `FirstName`, `SecondName`, `LastName`, `Username`, `Admission_No`, `Department`, `Year_of_Study` FROM `student` WHERE Username = '$_SESSION[login_user]'";
+		$result = mysqli_query($conn, $sql);
+	?>
 
-	<div class="tble">
+	<?php
+		$row = mysqli_fetch_array($result);
+	?>
+
+		<center><h1 style="padding-top: 30px; padding-bottom: 20px;">My Profile</h1></center>
+		<center><img src="img/p.png" style="border-radius: 50%; width: 150px; height: 150px;"></center><br>
+		<center>
 		<?php
-			$q = "SELECT `FirstName`, `SecondName`, `LastName`, `Username`, `Admission_No`, `Department`, `Year of Study` FROM `student` WHERE Username = '$_SESSION[login_user]'";
-
-			$res = mysqli_query($conn, $q) or die (mysqli_error($conn));
-
+			echo "Welcome"."&nbsp;" . strtoupper($_SESSION['login_user']);
 		?>
+		<br><br>
+		</center>
 
-		<?php
-			$row = mysqli_fetch_array($res);
-		?>
+		<center>
+		<table class="tbl_1">
 
-		<table>
-			<tr>
-				<td>FirstName</td>
-				<td><?php echo $row['FirstName'] ?></td>
-			</tr>
+		<tr>
+			<td>FirstName:</td>
+			<td><?php echo $row['FirstName'] ?></td>
+		</tr>
 
-			<tr>
-				<td>SecondName</td>
-				<td><?php echo $row['SecondName'] ?></td>
-			</tr>
+		<tr>
+			<td>SecondName:</td>
+			<td><?php echo $row['SecondName'] ?></td>
+		</tr>
 
-			<tr>
-				<td>LastName</td>
-				<td><?php echo $row['LastName'] ?></td>
-			</tr>
+		<tr>
+			<td>LastName:</td>
+			<td><?php echo $row['LastName'] ?></td>
+		</tr>
 
-			<tr>
-				<td>Username</td>
-				<td><?php echo $row['Username'] ?></td>
-			</tr>
+		<tr>
+			<td>Username:</td>
+			<td><?php echo $row['Username'] ?></td>
+		</tr>
 
-			<tr>
-				<td>Admission No</td>
-				<td><?php echo $row['Admission_No'] ?></td>
-			</tr>
+		<tr>
+			<td>Admission No:</td>
+			<td><?php echo $row['Admission_No'] ?></td>
+		</tr>
 
-			<tr>
-				<td>Department</td>
-				<td><?php echo $row['Department'] ?></td>
-			</tr>
+		<tr>
+			<td>Department:</td>
+			<td><?php echo $row['Department'] ?></td>
+		</tr>
 
-			<tr>
-				<td>Year of Study</td>
-				<td><?php echo $row['Year of Study'] ?></td>
-			</tr>
-		</table>
-	</div>
+		<tr>
+			<td>Year of study:</td>
+			<td><?php echo $row['Year_of_Study'] ?></td>
+		</tr>
+	</table>
+</center>
 </body>
 </html>
