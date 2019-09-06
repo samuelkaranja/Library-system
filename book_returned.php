78e0ef90bb8cd87b4bb480d7aeaf81a822d7f082
samuelkaranja <samuelkaranja007@gmail.com>
2019-09-06 13:32:53 +0300

    File update

--- book_returned.php
+++ book_returned.php
@@ -14,6 +14,7 @@
 	$sql = "SELECT student.Username, Admission_No, books.BookName, AuthorsName, Edition,book_requested.Approve, book_requested.Issue_Date, book_requested.Return_Date FROM student inner join book_requested ON student.Username = book_requested.Username inner join books ON book_requested.BookName = books.BookName WHERE book_requested.Approve = '$var1' ORDER BY book_requested.Return_Date ASC";
 
 	$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
+ 
  ?>
 
  <div>
